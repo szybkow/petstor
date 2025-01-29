d541661cd600c207a28c9af011accd344ca47a07
Maciej Stankiewicz <molejnik.it@gmail.com>
2025-01-29 03:55:09 +0100

    #PTS-012 implemented observable for REST METHODS

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -5,12 +5,31 @@ import { IPet } from '../interfaces/ipet';
 import { Status } from '../enums/status';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PetService {
-private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-  getPets(status: Status[]):Observable<IPet[]> {
-    return this.http.get<IPet[]>('https://petstore.swagger.io/v2/pet/findByStatus', {params: {status: status.join(',')}});
+  getPets(status: Status[]): Observable<IPet[]> {
+    return this.http.get<IPet[]>(
+      'https://petstore.swagger.io/v2/pet/findByStatus',
+      { params: { status: status.join(',') } }
+    );
+  }
+
+  getPetById(id: number): Observable<IPet> {
+    return this.http.get<IPet>('https://petstore.swagger.io/v2/pet/' + id);
+  }
+
+  addPet(pet: IPet): Observable<IPet> {
+    return this.http.post<IPet>('https://petstore.swagger.io/v2/pet', pet);
+  }
+
+  updatePet(pet: IPet): Observable<IPet> {
+    return this.http.put<IPet>('https://petstore.swagger.io/v2/pet', pet);
+  }
+
+  deletePet(id: number): Observable<void> {
+    return this.http.delete<void>('https://petstore.swagger.io/v2/pet/' + id);
   }
 }
